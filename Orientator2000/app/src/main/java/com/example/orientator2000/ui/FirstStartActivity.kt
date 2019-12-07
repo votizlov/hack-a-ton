50102ae58b468a61b527b10ead3c892a225e1fc3
Votizlov <seriousfox@yandex.ru>
2019-12-07 16:35:27 +0300

    added spinners

--- Orientator2000/app/src/main/java/com/example/orientator2000/ui/FirstStartActivity.kt
+++ Orientator2000/app/src/main/java/com/example/orientator2000/ui/FirstStartActivity.kt
@@ -3,6 +3,8 @@ package com.example.orientator2000.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.orientator2000.MainActivity
@@ -14,13 +16,37 @@ class FirstStartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_start)
+        setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+//        fab.setOnClickListener { view ->
             //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             //    .setAction("Action", null).show()
-            sendMessage(view)
+  //          sendMessage(view)
+   //     }
+        val spinner: Spinner = findViewById(R.id.spinner3)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.levels,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+        val spinner1: Spinner = findViewById(R.id.spinner4)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.ap,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner1.adapter = adapter
         }
     }
 
